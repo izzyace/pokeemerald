d48c92cdc76cedc5adfdc877e36f2fb1397d5171
LOuroboros <lunosouroboros@gmail.com>
2023-07-31 22:56:03 -0300

    Updated ItemUseOutOfBattle_PokeBall I made it handle the access to the party screen. Now I can forget about using the Poke Balls' .type item data field for that purpose, which in turn means that the only change required for the Poké Balls' item data now is to assign the out of battle effect. By doing this, the branch can keep working on clean copies of Pokeemerald like normal, and it can also work in up-to-date copies of the Pokeemerald-expansion too.

--- src/item_use.c
+++ src/item_use.c
@@ -1132,5 +1132,6 @@ void ItemUseOutOfBattle_CannotUse(u8 taskId)
 void ItemUseOutOfBattle_PokeBall(u8 taskId)
 {
     gItemUseCB = ItemUseCB_PokeBall;
-    SetUpItemUseCallback(taskId);
+    gBagMenu->newScreenCallback = CB2_ShowPartyMenuForItemUse;
+    Task_FadeAndCloseBagMenu(taskId);
 }
